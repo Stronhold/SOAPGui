f74ba6b407bb8d14a1578cb737b75d3f77d2e15c
stronhold <sergioausin1993@gmail.com>
2016-03-01 17:34:27 +0100

    Añadido nuevo servicio, arreglado el tema de enviar cosillas al servidor y cambio en el servidor SOAP

--- AseguradoraUI/MainWindow.xaml.cs
+++ AseguradoraUI/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using AseguradoraUI.ServicePolicy;
 
 namespace AseguradoraUI
 {
@@ -27,12 +28,43 @@ namespace AseguradoraUI
 
         private void FlowDocumentScrollViewer_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
         {
-
+            Console.WriteLine("Ha hecho click");
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            ServicePolicyClient ServicePolicy = new ServicePolicyClient();
+            Policy[] pol = ServicePolicy.GetAllPolicies();
+            RemoveRows();
+            AddRows(pol);
+        }
 
+        private void AddRows(Policy[] pol)
+        {
+            foreach (var policy in pol)
+            {
+                var rg = new TableRowGroup();
+                TableRow tR = new TableRow();
+                rg.Rows.Add(tR);
+                TableCell id = new TableCell(new Paragraph(new Run(policy.ID.ToString())));
+                TableCell name = new TableCell(new Paragraph(new Run(policy.Name)));
+                TableCell description = new TableCell(new Paragraph(new Run(policy.Description)));
+                id.TextAlignment = TextAlignment.Center;
+                name.TextAlignment = TextAlignment.Center;
+                description.TextAlignment = TextAlignment.Center;
+                tR.Cells.Add(id);
+                tR.Cells.Add(name);
+                tR.Cells.Add(description);
+                TablePoliza.RowGroups.Add(rg);
+            }
+        }
+
+        private void RemoveRows()
+        {
+            for(int i = 1; i < TablePoliza.RowGroups.Count; i++)
+            {
+                TablePoliza.RowGroups.RemoveAt(i);
+            }
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
@@ -42,6 +74,21 @@ namespace AseguradoraUI
 
         private void enviar_Click(object sender, RoutedEventArgs e)
         {
+            ServicePolicyClient ServicePolicy = new ServicePolicyClient();
+            Policy p = new Policy();
+            int ident = Convert.ToInt32(id.Text);
+            string n = name.Text;
+            string desc = description.Text;
+            bool added = ServicePolicy.AddPolicy(ident, n, desc);
+            if (!added)
+            {
+                MessageBox.Show("Error, your policy exists in the database", "Error adding to DB", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                MessageBox.Show("Added the new policy to our database", "Action completed", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            }
 
         }
     }
